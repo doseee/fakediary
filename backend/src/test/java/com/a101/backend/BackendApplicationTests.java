4391c3d7bc795fa5a61af2db7ac7b76112e26281
배진호 <qowlsgh99@naver.com>
2023-04-18 15:22:04 +0900

    🛠MISC : Backend ignored file upload

---
+++ backend/src/test/java/com/a101/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a101.fakediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
