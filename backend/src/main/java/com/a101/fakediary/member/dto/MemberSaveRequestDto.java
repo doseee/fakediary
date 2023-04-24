d57ec3196f3bef291af96d5f86e17e23cdc3db15
배진호 <qowlsgh99@naver.com>
2023-04-24 17:52:43 +0900

    ⭐Feat : "User CRUD init"

---
+++ backend/src/main/java/com/a101/fakediary/member/dto/MemberSaveRequestDto.java
@@ -0,0 +1,17 @@
+package com.a101.fakediary.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberSaveRequestDto {
+    private String email;
+
+    private String password;
+
+    private String nickname;
+}
--- backend/src/main/java/com/a101/fakediary/member/service/MemberService.java
+++ backend/src/main/java/com/a101/fakediary/member/service/MemberService.java
@@ -1,10 +1,21 @@
 package com.a101.fakediary.member.service;
 
+import com.a101.fakediary.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    //유저 회원가입
+
+
+
+
+
 }
