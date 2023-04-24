fe40cdae1fd7fd951a9b73d45b983d88396b43bf
배진호 <qowlsgh99@naver.com>
2023-04-24 10:35:01 +0900

    ⭐ FEAT : BaseEntity and Card Entity

--- backend/src/main/java/com/a101/fakediary/card/entity/Card.java
+++ backend/src/main/java/com/a101/fakediary/card/entity/Card.java
@@ -1,8 +1,11 @@
 package com.a101.fakediary.card.entity;
 
+import com.a101.fakediary.common.BaseEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 
 @Setter
 @Getter
@@ -10,5 +13,35 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @Builder
 @Entity
-public class Card {
+public class Card extends BaseEntity {
+
+    @SequenceGenerator(
+            name = "CARD_SEQ_GEN",
+            sequenceName = "CARD_SEQ",
+            initialValue = 100,
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CARD_SEQ_GEN")
+    private Long cardId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+
+    @Column(name = "keyword", length = 30, nullable = false)
+    private String keyword;
+
+    @Column(name = "langitude", nullable = true)
+    private BigDecimal langitude;
+
+    @Column(name = "longitude", nullable = true)
+    private BigDecimal longitude;
+
+    @Column(name = "origin_card_image_url", nullable = false)
+    private String originCardImageUrl;
+
+    @Column(name = "card_image_url", nullable = false)
+    private String originCardImage;
 }
---
+++ backend/src/main/java/com/a101/fakediary/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.a101.fakediary.common;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
