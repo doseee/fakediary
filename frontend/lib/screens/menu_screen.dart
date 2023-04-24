37adfeb9866fb8e09a6ddaa2411aeede93322a54
문성현 <forest7592@hanmail.net>
2023-04-24 15:50:37 +0900

    ⭐ FEAT : Make menu screen

---
+++ frontend/lib/screens/menu_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/bg_galaxy.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: const [
+            Flexible(
+              flex: 100,
+              child: Text('top'),
+            ),
+            Flexible(
+              flex: 427,
+              child: Text('top'),
+            ),
+            Flexible(
+              flex: 318,
+              child: Text('top'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
