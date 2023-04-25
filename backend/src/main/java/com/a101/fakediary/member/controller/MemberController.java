ff1b0fdcb3d00214fec2d5b9bcd8e6e97d6b9712
배진호 <qowlsgh99@naver.com>
2023-04-25 15:46:13 +0900

    ⭐ Feat : Completion of implementation of c and d while implementing member crud

--- backend/src/main/java/com/a101/fakediary/member/controller/MemberController.java
+++ backend/src/main/java/com/a101/fakediary/member/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.a101.fakediary.member.controller;
 
-import com.a101.fakediary.member.dto.MemberLoginRequestDto;
 import com.a101.fakediary.member.dto.MemberSaveRequestDto;
+import com.a101.fakediary.member.dto.MemberUpdateRequestDto;
+import com.a101.fakediary.member.entity.Member;
+import com.a101.fakediary.member.repository.MemberRepository;
 import com.a101.fakediary.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RequestMapping("/member")
@@ -13,20 +19,47 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody MemberSaveRequestDto memberSaveRequestDto){
-        System.out.println("하이");
+
         if(memberSaveRequestDto == null){
             return ResponseEntity.badRequest().body("MemberSaveRequestDto is null");
         }
-        return memberService.MemberSignUp(memberSaveRequestDto);
+        return memberService.signUpMember(memberSaveRequestDto);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password){
 
-        return memberService.MemberLogin(email, password);
+        return memberService.signInMember(email, password);
+    }
+
+//    @PatchMapping("/{id}")
+//    public ResponseEntity<?> updateMember(@PathVariable Long id, @RequestBody MemberUpdateRequestDto requestDto) {
+//        boolean isNicknameUnique = memberService.isNicknameUnique(requestDto.getNickname());
+//        if (!isNicknameUnique) {
+//            return ResponseEntity.badRequest().body("Nickname already exists.");
+//        }
+//
+//        Optional<Member> member = memberService.findMemberById(id);
+//        if (member.isPresent()) {
+//            memberService.updateMember(id, requestDto);
+//            return ResponseEntity.ok().build();
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<?> deleteMember(@PathVariable Long memberId){
+
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+        if (!memberOptional.isPresent()) {
+            return ResponseEntity.badRequest().body("Invalid memberId");
+        }
+
+        return memberService.removeMember(memberId);
     }
 
 
--- backend/src/main/java/com/a101/fakediary/member/dto/MemberLoginRequestDto.java
+++ backend/src/main/java/com/a101/fakediary/member/dto/MemberLoginRequestDto.java
@@ -1,5 +1,13 @@
 package com.a101.fakediary.member.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MemberLoginRequestDto {
     private String email;
     private String password;
