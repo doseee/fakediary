29e550acd97d74698b62e622256ab7f71f128c87
choi da hye <traveler9804@gmail.com>
2023-04-25 15:14:36 +0900

    ⭐FEAT : Add login
    로그인 화면
    
    RESOLVES : #S08P31A101-389

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/login.dart';
 import 'package:frontend/screens/login_entrance.dart';
 import 'package:frontend/screens/menu_screen.dart';
 
