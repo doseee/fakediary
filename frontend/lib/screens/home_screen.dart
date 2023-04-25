60c8dd4136480db562d24a1d587fd0d42b526f34
박소정 <sjcoder9611@naver.com>
2023-04-25 14:56:05 +0900

    ⭐ FEAT : main page
    develop pull 하기 전 커밋
    
    RESOLVES:

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/menu_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,49 +12,53 @@ class _MainScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/img/main_background.jpg'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          toolbarHeight: MediaQuery.of(context).size.height * 0.1183,
+          actions: const [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/img/icon_alarm.png'),
+                        width: 50, // set the desired width
+                        height: 50, // set the desired height
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MenuScreen()),
+            );
+          },
+          image: const DecorationImage(
             image: AssetImage('assets/img/main_background.jpg'),
           ),
         ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            toolbarHeight: MediaQuery.of(context).size.height * 0.1183,
-            leading: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Image(
-                image: AssetImage('assets/img/icon_menu.png'),
-                width: 80,
-                height: 80,
-              ),
-            ),
-            actions: const [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 30,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Image(
-                          image: AssetImage('assets/img/icon_alarm.png'),
-                          width: 50, // set the desired width
-                          height: 50, // set the desired height
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
