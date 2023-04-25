70feff6a90ef232f47f828cea9b75d1566de7380
choi da hye <traveler9804@gmail.com>
2023-04-25 14:53:32 +0900

    ⭐FEAT : Add login entrance
    로그인, 회원가입 안내 화면
    
    RESOLVES : #S08P31A101-389 , #S08P31A101-390

---
+++ frontend/lib/screens/login_entrance.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/main.dart';
+import 'package:lottie/lottie.dart';
+
+import 'menu_screen.dart';
+
+class LoginEntrance extends StatefulWidget {
+  const LoginEntrance({Key? key}) : super(key: key);
+
+  @override
+  State<LoginEntrance> createState() => _LoginEntranceState();
+}
+
+class _LoginEntranceState extends State<LoginEntrance> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/img/bg_galaxy.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            toolbarHeight: MediaQuery.of(context).size.height * 0.1183,
+            title: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MenuScreen(),
+                  ),
+                );
+              },
+              icon: Image.asset('assets/img/icon_menu.png'),
+              iconSize: 50,
+            ),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Flexible(flex: 3,
+                  child:
+                  Center(
+                    child: Text('My Lieary', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24, color: Color(0xfff1f1f1)),
+                ),),),
+              Flexible(flex: 4, child: Container(
+                child: Lottie.asset('assets/lottie/login_ent.json', width: 300),
+              )),
+              Padding(padding: EdgeInsets.only(left: 50, right: 50),
+                child:
+                Flexible(flex: 1, child: ElevatedButton(
+                  onPressed: () { print('s'); },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffffffff),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    )
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(100, 10, 100, 10),
+                    child:
+                        Text('LOGIN', style: TextStyle(color: Colors.black, fontSize: 14),)
+                  )
+                ),),),
+                Padding(padding: EdgeInsets.only(left: 50, right: 50),
+                  child:
+                  Flexible(flex: 1, child: ElevatedButton(
+                      onPressed: () { print('join'); },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff4b6858),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          )
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(100, 10, 100, 10),
+                          child:
+                          Text('JOIN', style: TextStyle(color: Colors.white, fontSize: 14),)
+                      )
+                  ),),),
+                Flexible(flex: 2, child: Container()),
+              ],
+            )
+          )
+      ),
+    );
+  }
+}
