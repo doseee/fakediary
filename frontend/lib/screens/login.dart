88d48eb2cce8320f8855c536e03536e52da44608
choi dahye <traveler9804@gmail.com>
2023-04-26 23:49:25 +0900

    🛠FIX : login layout & api
    로그인 api, layout 수정했습니다
    로그인 가능
    
    RESOLVES :#S08P31A101-389

--- frontend/lib/screens/login.dart
+++ frontend/lib/screens/login.dart
@@ -18,7 +18,7 @@ class _LoginState extends State<Login> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  Widget _userIdWidget(){
+  Widget _userIdWidget() {
     return TextFormField(
       controller: _emailController,
       keyboardType: TextInputType.emailAddress,
@@ -37,16 +37,17 @@ class _LoginState extends State<Login> {
           borderSide: BorderSide(color: Colors.greenAccent),
         ),
       ),
-      validator: (String? value){
-        if (value!.isEmpty){
+      validator: (String? value) {
+        if (value!.isEmpty) {
           return '이메일이 공백입니다';
         }
         return null;
       },
-    style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),);
+      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+    );
   }
 
-  Widget _passwordWidget(){
+  Widget _passwordWidget() {
     return TextFormField(
       controller: _passwordController,
       obscureText: true,
@@ -65,132 +66,109 @@ class _LoginState extends State<Login> {
           borderSide: BorderSide(color: Colors.greenAccent),
         ),
       ),
-      validator: (String? value){
+      validator: (String? value) {
         if (value!.isEmpty) {
           return '비밀번호가 공백입니다';
         }
         return null;
       },
-      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),);
+      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+    );
   }
 
-  Widget _buttonLogin(){
-    return Center(
-        child: Container(
-        width: 250,
-        height: 50,
-        decoration: BoxDecoration(
-        gradient: LinearGradient(
-        colors: [
-        Color(0xff79F1A4),
-        Color(0xff0E5CAD),
-    ]
-    ),
-    borderRadius: BorderRadius.circular(25)
-    ),
-    child: Flexible(flex: 1, child: ElevatedButton(
-    onPressed: () { Navigator.push(
-    context,
-    MaterialPageRoute(
-    builder: (context) => const Login(),
-    ));},
-    style: ElevatedButton.styleFrom(
-    backgroundColor: Colors.transparent,
-    shadowColor: Colors.transparent,
-    elevation: 0.0,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(30.0),
-    )
-    ),
-    child:
-    Text('LOGIN', style: TextStyle(color: Colors.white, fontSize: 14),)
-    ),)));
+  Widget _buttonLogin() {
+    return GestureDetector(
+      onTap: () async {
+        if (_formKey.currentState!.validate()) {
+          print(_emailController.text);
+          print(_passwordController.text);
+          final bool result = await ApiService.login(
+              _emailController.text,
+              _passwordController.text);
+          if (!mounted) return;
+          if (result) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        MainScreen()));
+          }
+        }
+      },
+      child: Center(
+          child: Container(
+            width: 250,
+            height: 50,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0xff79F1A4),
+                  Color(0xff0E5CAD),
+                ]),
+                borderRadius: BorderRadius.circular(25)),
+            child: Center(
+            child: Text(
+              'LOGIN',
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            )),
+            ),
+          ));
+
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xff0F2027),
-                Color(0xff203A43),
-                Color(0xff2C5364),
-              ],
-              stops: [0, 0.4, 1.0],
-            ),
-            image: DecorationImage(
-              image: AssetImage('assets/img/main_background.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Scaffold(
-              resizeToAvoidBottomInset: false,
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xff0F2027),
+                  Color(0xff203A43),
+                  Color(0xff2C5364),
+                ],
+                stops: [0, 0.4, 1.0],
               ),
-              body: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Flexible(flex: 3,child: Container(
-                      child: Center(
-                        child: Lottie.asset('assets/lottie/stars.json'),
-                      ),
-                    ),
-                    ),
-                    Flexible(flex: 12,child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Flexible(flex: 3, child: Column(
-                          children: [
-                            Flexible(flex: 2,
-                                child: Padding(
-                                  padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                                  child: Column(
-                                    children: [
-                                      _userIdWidget(),
-                                      SizedBox(height: 20,),
-                                      _passwordWidget(),
-                                      SizedBox(height: 20,),
-                                      GestureDetector(
-                                        onTap: () async {
-                                          if (_formKey.currentState!.validate()) {
-                                            final bool result = await ApiService.login(_emailController.text, _passwordController.text);
-                                            if (!mounted) return;
-                                            if (result) {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) => MainScreen()));
-                                            }
-                                          }
-                                        },
-                                        child: _buttonLogin(),
-                                      )
-                                    ],
-                                  ),
-                                )),
-                          ],
-                        )),
-                        Flexible(flex: 1, child: Text('My Lieary', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w800),),)
-                      ],
-                    ),
-                    ),
-                  ],
+              image: DecorationImage(
+                image: AssetImage('assets/img/main_background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Scaffold(
+                resizeToAvoidBottomInset: false,
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
                 ),
-              )
-          )
-      ),
-    );
+                body: Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 80),
+                      child: Column(children: [
+                        Center(
+                          child: Lottie.asset('assets/lottie/stars.json'),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            _userIdWidget(),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            _passwordWidget(),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            _buttonLogin()
+                          ],
+                        ),
+                      ]),
+                    )))));
   }
 }
