6864c48d28e300b019a6dc20e249ae0a43f81ced
박소정 <sjcoder9611@naver.com>
2023-05-02 02:19:33 +0900

    ⭐FEAT : 카드 선택 페이지 ui 2차
    클릭시 상단 나타남, 클릭시 변화주기
    
    RESOLVES: #S08P31A101-569

--- frontend/lib/screens/diary_create_cards.dart
+++ frontend/lib/screens/diary_create_cards.dart
@@ -10,21 +10,24 @@ class DiaryCreateCards extends StatefulWidget {
   @override
   _DiaryCreateState createState() => _DiaryCreateState();
 }
+
 class _DiaryCreateState extends State<DiaryCreateCards> {
-  List temp = ["a","b","a","b","a","b"];
+  List temp = ["a", "b", "c", "d", "e", "b"];
+  List<String> selectedCards = [];
 
   @override
   void initState() {
     super.initState();
     initId();
   }
+
   late int memberId;
+
   initId() async {
     final prefs = await SharedPreferences.getInstance();
     memberId = prefs.getInt('memberId')!;
     print(memberId);
-  await ApiService().getCards(memberId);
-
+    await ApiService().getCards(memberId);
   }
 
   Widget build(BuildContext context) {
@@ -34,15 +37,15 @@ class _DiaryCreateState extends State<DiaryCreateCards> {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 stops: [
-                  0.3,
-                  0.6,
-                  0.9
-                ],
+              0.3,
+              0.6,
+              0.9
+            ],
                 colors: [
-                  Color(0xff0f2027),
-                  Color(0xff203a43),
-                  Color(0xff2c5364),
-                ])),
+              Color(0xff0f2027),
+              Color(0xff203a43),
+              Color(0xff2c5364),
+            ])),
         child: Scaffold(
             backgroundColor: Colors.transparent,
             appBar: AppBar(
@@ -52,17 +55,19 @@ class _DiaryCreateState extends State<DiaryCreateCards> {
               actions: [
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-
                   children: [
                     Padding(
                       padding: EdgeInsets.symmetric(horizontal: 30),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Text('CARDS  ', style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),),
+                          Text(
+                            'CARDS  ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
                           GestureDetector(
                             onTap: () {
                               Navigator.push(
@@ -86,123 +91,166 @@ class _DiaryCreateState extends State<DiaryCreateCards> {
             ),
             body: Center(
                 child: Column(children: [
-                  Flexible(flex:1, child:Scaffold(backgroundColor: Colors.transparent,)),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(width: 1, color: Colors.white),
-                        ),
-                      ),
-                      child: Scaffold(
-                        backgroundColor: Colors.transparent,
-                        body: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(height: 15),
-                              Text(
-                                '이야기로 만들 기억의 조각을 선택하세요.',
-                                style: TextStyle(fontSize: 10, color: Colors.white),
+              Flexible(
+                flex: 1,
+                child: GridView.count(
+                  crossAxisCount: 5,
+                  childAspectRatio: 4,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 1.0,
+                  padding: EdgeInsets.all(5.0),
+                  children: List.generate(
+                    selectedCards.length,
+                    (index) {
+                      return Card(
+                        color: Colors.transparent,
+                        elevation: 0.0,
+                        child: Column(
+                          children: <Widget>[
+                            Image(
+                              image:
+                                  AssetImage('assets/img/cardlist_tmpcard.png'),
+                              height: 50,
+                              width: 50,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                selectedCards[index],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12),
                               ),
-                              SizedBox(height: 10),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(width: 1, color: Colors.white),
+                    ),
+                  ),
+                  child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    body: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 15),
+                          Text(
+                            '이야기로 만들 기억의 조각을 선택하세요.',
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                          SizedBox(height: 10),
+                        ],
                       ),
                     ),
                   ),
-                  Flexible(
-                      flex: 3,
-                      child: GridView.count(
-                        crossAxisCount: 3,
-                        // 가로 방향으로 3개의 카드씩 표시
-                        childAspectRatio: 0.7,
-                        // 카드의 가로 세로 비율 설정
-                        mainAxisSpacing: 10.0,
-                        // 카드들의 세로 간격 설정
-                        crossAxisSpacing: 10.0,
-                        // 카드들의 가로 간격 설정
-                        padding: EdgeInsets.all(10.0),
-                        // GridView 자체의 Padding 설정
-                        children: List.generate(
-                          // 카드 리스트 생성
-                          temp.length, // 총 카드 갯수
-                              (index) {
-                            return InkWell(
-
-                                child: Card(
-                                  color: Colors.transparent,
-                                  elevation: 0.0,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Image(
-                                        image: AssetImage(
-                                            'assets/img/cardlist_tmpcard.png'),
-                                        height: 110,
-                                        width: 110,
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(temp[index],
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 12)),
-                                      ),
-                                    ],
-                                  ),
-                                ));
-                          },
-                        ),
-                      )),
-                Flexible(
-                    flex: 1,
-
-                    child: GestureDetector(
-                      onTap :() {
-                        // navigate to the desired class
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => MoodSelect()),
-                        );
-                      },
-
-                    child : Container(
-                      width: 268,
-                      height: 61,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            const Color(0xff263344),
-                            const Color(0xff1B2532).withOpacity(0.53),
-                            const Color(0xff1C2A3D).withOpacity(0.5),
-                            const Color(0xff1E2E42).withOpacity(0.46),
-                            const Color(0xff364B66).withOpacity(0.33),
-                            const Color(0xff2471D6).withOpacity(0),
-                          ],
-                          stops: const [0, 0.25, 0.4, 0.5, 0.75, 1.0],
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0xff000000).withOpacity(0.25),
-                            offset: const Offset(0, 4),
-                            blurRadius: 4,
+                ),
+              ),
+              Flexible(
+                flex: 4,
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  childAspectRatio: 0.7,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  padding: EdgeInsets.all(10.0),
+                  children: List.generate(
+                    temp.length,
+                    (index) {
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            if (selectedCards.contains(temp[index])) {
+                              selectedCards.remove(temp[index]);
+                            } else {
+                              selectedCards.add(temp[index]);
+                            }
+                          });
+                        },
+                        child: Card(
+                          color: selectedCards.contains(temp[index])
+                              ? Colors.blue
+                              : Colors.transparent,
+                          elevation: 0.0,
+                          child: Column(
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage(
+                                    'assets/img/cardlist_tmpcard.png'),
+                                height: 110,
+                                width: 110,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  temp[index],
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: const Center(
-                       child:  Text(
-                          'NEXT',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),)
-
-                      ),)
-                    )
-                ])))));
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: GestureDetector(
+                    onTap: () {
+                      // navigate to the desired class
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MoodSelect()),
+                      );
+                    },
+                    child: Container(
+                        width: 268,
+                        height: 61,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              const Color(0xff263344),
+                              const Color(0xff1B2532).withOpacity(0.53),
+                              const Color(0xff1C2A3D).withOpacity(0.5),
+                              const Color(0xff1E2E42).withOpacity(0.46),
+                              const Color(0xff364B66).withOpacity(0.33),
+                              const Color(0xff2471D6).withOpacity(0),
+                            ],
+                            stops: const [0, 0.25, 0.4, 0.5, 0.75, 1.0],
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color(0xff000000).withOpacity(0.25),
+                              offset: const Offset(0, 4),
+                              blurRadius: 4,
+                            ),
+                          ],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'NEXT',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        )),
+                  ))
+            ])))));
   }
 }
