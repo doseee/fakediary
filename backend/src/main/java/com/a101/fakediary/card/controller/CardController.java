d72fa94db74c36f5a7952b23eb7c26f3bd8e0cd6
배진호 <qowlsgh99@naver.com>
2023-05-12 16:03:21 +0900

    🛠FIX : 자동생성 간격 30분으로 변경, findCard!!!! log 주석처리

--- backend/src/main/java/com/a101/fakediary/card/controller/CardController.java
+++ backend/src/main/java/com/a101/fakediary/card/controller/CardController.java
@@ -88,7 +88,7 @@ public class CardController {
      */
     @GetMapping("/pick/{cardId}")
     public ResponseEntity<?> findCard(@PathVariable(name = "cardId")Long cardId) {
-        log.info("findCard!!!!");
+//        log.info("findCard!!!!"); 너무 지저분하게 많이떠서 주석처리
         CardSaveResponseDto cardResponseDto = null;
 
         try {
