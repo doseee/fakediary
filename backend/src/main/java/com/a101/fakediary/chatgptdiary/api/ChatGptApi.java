fb7a8c933b8e3628284dcfd29d2c9aafe8930804
jwelyl <cork2586@naver.com>
2023-05-13 21:05:50 +0900

    temporary

--- backend/src/main/java/com/a101/fakediary/chatgptdiary/api/ChatGptApi.java
+++ backend/src/main/java/com/a101/fakediary/chatgptdiary/api/ChatGptApi.java
@@ -4,11 +4,8 @@ import com.a101.fakediary.chatgptdiary.dto.message.Message;
 import com.a101.fakediary.chatgptdiary.dto.request.ChatGptDiaryRequestDto;
 
 import com.a101.fakediary.chatgptdiary.dto.response.ChatGptDiaryResponseDto;
-import com.a101.fakediary.chatgptdiary.dto.result.DiaryResultDto;
-import com.a101.fakediary.chatgptdiary.dto.result.TitleSubtitlesResultDto;
 import com.a101.fakediary.chatgptdiary.prompt.ChatGptPrompts;
 import com.a101.fakediary.chatgptdiary.config.ChatGptApiConfig;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,7 +13,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -52,51 +48,6 @@ public class ChatGptApi {
 
     /**
      * 
-     * @param prompt : 등장인물, 장소, 키워드, 장르를 넣고 title과 subtitles를 얻어내는 프롬프트
-     * @return : 제목과 내용
-     * @throws Exception
-     */
-    public TitleSubtitlesResultDto askGpt4TitleSubtitles(String prompt) throws Exception {
-        Instant start = Instant.now();
-
-        List<Message> messages = new ArrayList<>();
-        messages.add(new Message("system", ChatGptPrompts.generateSystemPromptTileSubtitles()));
-
-        messages.add(new Message("user", prompt));
-
-        ChatGptDiaryRequestDto requestDto = ChatGptDiaryRequestDto.builder()
-                .model(MODEL_4_0)
-                .n(N)
-                .maxTokens(MAX_TOKENS_4_0)
-                .temperature(TEMPERATURE)
-                .messages(messages)
-                .build();
-
-        ChatGptDiaryResponseDto responseDto = restTemplate40.postForObject(API_URL, requestDto, ChatGptDiaryResponseDto.class);
-
-        if(responseDto == null || responseDto.getChoices() == null || responseDto.getChoices().isEmpty()) {
-            log.info("no response!!!");
-            throw new Exception("GPT4가 응답이 없음");
-        }
-
-        log.info("responseDto = " + responseDto);
-        String responseDtoString = responseDto.getChoices().get(0).getMessage().getContent().trim();
-
-//        log.info("responseDtoString = " + responseDtoString);
-//        responseDtoString = "{" + responseDtoString + "}";
-        log.info("responseDtoString = " + responseDtoString);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        TitleSubtitlesResultDto dto = objectMapper.readValue(responseDtoString, TitleSubtitlesResultDto.class);
-
-        Instant end = Instant.now();
-        log.info("GPT4 키워드로 일기 제목, 내용 받아오는데 걸리는 소요 시간 : " + Duration.between(start, end).toMillis() + " ms");
-
-        return dto;
-    }
-    
-    /**
-     * 
      * @param messages : 전체 프롬프트 Message
      * @param prompt   : 이번 질의에서 추가될 프롬프트 문자열
      * @return : gpt4가 만들어준 대답 프롬프트
@@ -105,7 +56,7 @@ public class ChatGptApi {
         Instant start = Instant.now();
 
         if(messages.isEmpty()) {
-            messages.add(new Message("system", ChatGptPrompts.generateSystemPrompt1()));
+            messages.add(new Message("system", ChatGptPrompts.generateSystemPrompt()));
         }
 
         messages.add(new Message("user", prompt));
@@ -130,49 +81,6 @@ public class ChatGptApi {
         
         if(!answer.endsWith("}")) {
             messages = askGpt41(messages, ChatGptPrompts.generateUserContinuePrompt());
-        }
-
-        Instant end = Instant.now();
-        log.info("GPT4 키워드로 일기 내용 받아오는데 걸리는 소요 시간 : " + Duration.between(start, end).toMillis() + " ms");
-
-        return messages;
-    }
-
-    /**
-     *
-     * @param messages : 전체 프롬프트 Message
-     * @param prompt   : 이번 질의에서 추가될 프롬프트 문자열
-     * @return : gpt4가 만들어준 대답 프롬프트
-     */
-    public List<Message> askGpt42(List<Message> messages, String prompt) throws Exception {
-        Instant start = Instant.now();
-
-        if(messages.isEmpty()) {
-            messages.add(new Message("system", ChatGptPrompts.generateSystemPrompt2()));
-        }
-
-        messages.add(new Message("user", prompt));
-
-        ChatGptDiaryRequestDto requestDto = ChatGptDiaryRequestDto.builder()
-                .model(MODEL_4_0)
-                .n(N)
-                .maxTokens(MAX_TOKENS_4_0)
-                .temperature(TEMPERATURE)
-                .messages(messages)
-                .build();
-
-        ChatGptDiaryResponseDto responseDto = restTemplate40.postForObject(API_URL, requestDto, ChatGptDiaryResponseDto.class);
-
-        if(responseDto == null || responseDto.getChoices() == null || responseDto.getChoices().isEmpty()) {
-            log.info("no response!!!");
-            throw new Exception("GPT4가 응답이 없음");
-        }
-
-        String answer = responseDto.getChoices().get(0).getMessage().getContent().trim();
-        messages.add(new Message("assistant", answer));
-
-        if(!answer.endsWith("}")) {
-            messages = askGpt42(messages, ChatGptPrompts.generateUserContinuePrompt());
         }
 
         Instant end = Instant.now();
