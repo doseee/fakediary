f4319a2b235576d62cc7ea09fa3228aa53ec580d
jwelyl <cork2586@naver.com>
2023-05-14 23:11:34 +0900

    ⭐ FEAT : 영어 -> 한국어 번역 로직 추가
    Consist of PapagoTranslator.java, PapagoApi.java
    
    RESOLVES : #S08P31A101-912"

--- backend/src/main/java/com/a101/fakediary/papago/api/PapagoApi.java
+++ backend/src/main/java/com/a101/fakediary/papago/api/PapagoApi.java
@@ -1,12 +1,10 @@
 package com.a101.fakediary.papago.api;
 
-import com.a101.fakediary.diary.service.DiaryService;
 import com.a101.fakediary.papago.service.PapagoTranslator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Matcher;
@@ -20,8 +18,20 @@ public class PapagoApi {
     private static final Logger logger = LoggerFactory.getLogger(PapagoApi.class);
 
 //    @Async 자동생성 에러내던 주범 으아아악
-    public String translate(String text) { //메소드 불러서 바꾸고 싶은 내용 text에 넣으면 한 -> 영 바꿔서 return
-        String translatedText = papagoTranslator.translate(text).block();
+    public String translateKorToEng(String text) { //메소드 불러서 바꾸고 싶은 내용 text에 넣으면 한 -> 영 바꿔서 return
+        String translatedText = papagoTranslator.translateKorToEng(text).block();
+        Pattern pattern = Pattern.compile("\"translatedText\":\"([^\"]*)\"");
+        Matcher matcher = pattern.matcher(translatedText);
+        if (matcher.find()) {
+            String trans = matcher.group(1);
+            logger.info("번역완료 : {} -> {}", text, trans);
+            return trans;
+        }
+        return null;
+    }
+
+    public String translateEngToKor(String text) { //메소드 불러서 바꾸고 싶은 내용 text에 넣으면 한 -> 영 바꿔서 return
+        String translatedText = papagoTranslator.translateEngToKor(text).block();
         Pattern pattern = Pattern.compile("\"translatedText\":\"([^\"]*)\"");
         Matcher matcher = pattern.matcher(translatedText);
         if (matcher.find()) {
