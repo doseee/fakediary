e800f1228d443d163087c90bbb077be0f85bf69e
허재성 <cork2586@naver.com>
2023-05-17 08:54:21 +0900

    🛠 FIX : Diary의 음악 URL column nullable하게 수정
    Consist of Diary.java
    
    RESOLVE : #S08P3A101-868

--- backend/src/main/java/com/a101/fakediary/diary/entity/Diary.java
+++ backend/src/main/java/com/a101/fakediary/diary/entity/Diary.java
@@ -59,7 +59,7 @@ public class Diary extends BaseEntity {
     @Column(nullable = false, length = 1000)
     private String summary;
 
-    @Column(name = "diary_music_url", nullable = false)
+    @Column(name = "diary_music_url")
     private String diaryMusicUrl;
 
     @Column(nullable = false)
