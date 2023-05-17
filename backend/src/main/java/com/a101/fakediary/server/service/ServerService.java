b71850b432c0238823dbd4c14e8aa7e98b4da9ef
배진호 <qowlsgh99@naver.com>
2023-05-17 10:54:37 +0900

    ⭐FEAT : 현재 돌고있는 스프링 포트 찾는 API 기능 서비스로 분리

--- backend/src/main/java/com/a101/fakediary/server/service/ServerService.java
+++ backend/src/main/java/com/a101/fakediary/server/service/ServerService.java
@@ -1,6 +1,8 @@
 package com.a101.fakediary.server.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +10,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class ServerService {
+
+    private final Environment environment;
+
+    public String findServerPort() {
+        String port = environment.getProperty("local.server.port");
+        if (port != null)
+            return port;
+        else
+            throw new RuntimeException("getServerPort Failed");
+    }
 }
