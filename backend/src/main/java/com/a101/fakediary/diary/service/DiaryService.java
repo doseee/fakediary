a860bfe789cd3de03bcfe2c121a7568243b77eb2
jwelyl <cork2586@naver.com>
2023-05-28 12:55:16 +0900

    🛠 FIX : 테스트 일기 장르 변경
    Consist of DiaryService.java
    
    RESOLVE : #S08P3A101-868

--- backend/src/main/java/com/a101/fakediary/diary/service/DiaryService.java
+++ backend/src/main/java/com/a101/fakediary/diary/service/DiaryService.java
@@ -734,9 +734,10 @@ public class DiaryService {
         return diaryResultDto;
     }
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public void createTestDiaries() throws Exception {
         List<String> genreList = new ArrayList<>();
-        genreList.add("HAPPY");
+        genreList.add("TOUCHING");
         genreList.add("WARM");
 
         for(long cardId = 618L; cardId < 660L; cardId++) {
